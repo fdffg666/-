77707bc7e6745af1699a94020b71c326004d1675
fdffg666 <1376406630@qq.com>
2024-10-24 10:19:40 +0800

    用100元买100书，判断每种书有多少本

---
+++ 用100元买100书，判断每种书有多少本/FileName.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a, b, c, d, f;
+	f = 100;//这是总本数
+	for (a = 0; a <= f / 5; a++) {
+		for (b = 0; b <= (f - a) / 3; b++) {
+			c = 100 - a - b;
+			if (((5 * a) + (3 * b) + c/3) == f)// 总本数=总钱数
+			{
+				cout << "大本数" << a << "小本数" << b << "小小本数" << c << endl;
+			}
+			
+		}
+	}
+	cout << "2412402060718 卿俊伟" << endl;
+	return 0;
+}
